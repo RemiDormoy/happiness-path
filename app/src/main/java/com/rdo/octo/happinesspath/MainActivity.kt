15dd0e2ccefc4b264b19b45d1110ec1c3feec7df
Remi Dormoy <rdormoy@octo.com>
2019-08-13 11:31:28 +0200

    initial commit

---
+++ app/src/main/java/com/rdo/octo/happinesspath/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.rdo.octo.happinesspath
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        textView.setOnClickListener {
+            startActivity(Intent(this, OperationsActivity::class.java))
+        }
+    }
+}
---
+++ app/src/main/java/com/rdo/octo/happinesspath/OperationsActivity.kt
@@ -0,0 +1,14 @@
+package com.rdo.octo.happinesspath
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class OperationsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_operations)
+
+
+    }
+}
