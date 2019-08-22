a6214fefe7fbe1c5f4c4d98c9e21c3cb1de60213
Remi Dormoy <rdormoy@octo.com>
2019-08-22 14:02:44 +0200

    added confirm screen

--- app/src/main/java/com/rdo/octo/happinesspath/TransferActivity.kt
+++ app/src/main/java/com/rdo/octo/happinesspath/TransferActivity.kt
@@ -113,6 +113,22 @@ class TransferActivity : AppCompatActivity() {
 
                 override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
             })
+
+            reasonEditText.addTextChangedListener(object : TextWatcher {
+                override fun afterTextChanged(s: Editable) {
+                    reasonCountTextView.text = "${reasonEditText.text.length}/120"
+                }
+
+                override fun beforeTextChanged(
+                    s: CharSequence,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+                }
+
+                override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+            })
         }
     }
 
